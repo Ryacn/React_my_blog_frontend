4f7c33d49814f292f0d36cecb2de4ff3128b1933
Ryan <sora031@gmail.com>
2019-09-17 23:30:48 -0700

    first init

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <>
+        <h1>Hello, about</h1>
+        <p>
+            blog test
+            </p>
+    </>
+);
+export default AboutPage; 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+        <h1>Hello, testing</h1>
+        <p>
+            blog test
+            </p>
+    </>
+);
+export default HomePage; 
